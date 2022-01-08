89cd11e81119b75031aed81e3fda82899af5a148
yangwang000 <wymm.010@gmail.com>
2022-01-08 12:05:24 -0500

    update

--- src/main/java/com/java/adawang/MustHaveKnowledge/DataStructures/ArraysStrings.java
+++ src/main/java/com/java/adawang/MustHaveKnowledge/DataStructures/ArraysStrings.java
@@ -7,7 +7,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 
-public class ArraysStrings {
+class ArraysStrings {
 	class IsUnique{
 		//Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?
 		public boolean isUniqueSet(String s){
@@ -60,5 +60,105 @@ public class ArraysStrings {
 		}
 		end = System.currentTimeMillis();
 		System.out.println(setRes+ " " + (end-start));
+	}
+
+	class CheckPermutation{
+		//Given two strings, write a method to decide if one is a permutation of the other.
+		// Case sensitive and whitespace is significant
+		// so "God  " is different from "dog"
+		String sort(String s){
+			char[] content = s.toCharArray();
+			java.util.Arrays.sort(content);
+			return new String(content);
+		}
+
+		public boolean checkPermutationBySorting(String s
+				, String t){
+			if(s.length() != t.length())
+				return false;
+			return sort(s).equals(sort(t));
+		}
+
+		public boolean checkPermutationByCharset(String s
+				, String t){
+			if(s.length() != t.length())
+				return false;
+
+			// letters array`s length depends on ASCII or
+			// Unicode
+			int[] letters = new int[128];
+			for(char c : s.toCharArray()){
+				letters[c]++;
+			}
+
+			for(int i=0; i < t.length(); i++){
+				int c = (int) t.charAt(i);
+				letters[c]--;
+				if(letters[c] < 0)
+					return false;
+			}
+			return true;
+		}
+	}
+
+	class CheckPalindromePermutation{
+		// Given a string, write a function to check if
+		// it is a permutation of a palindrome. "Tact
+		// Coa" will return true.
+		public boolean checkByOddCounts(String s){
+			int countOdd = 0;
+			int[] charTable =
+					new int[Character.getNumericValue('z') - Character.getNumericValue('a') + 1];
+			for(char c : s.toCharArray()){
+				int x = getCharNumber(c);
+				if(x != -1){
+					charTable[x]++;
+					if(charTable[x]%2 == 1)
+						countOdd++;
+					else
+						countOdd--;
+				}
+			}
+			return countOdd <= 1;
+		}
+
+		int getCharNumber(Character c){
+			int a = Character.getNumericValue('a');
+			int z = Character.getNumericValue('z');
+			int val = Character.getNumericValue(c);
+			if(a <= val && val <= z){
+				return val - a;
+			}
+			return -1;
+		}
+
+		public boolean checkByBitVector(String s){
+			int bitVector = creatBitVector(s);
+			return checkExactlyOneBitSet(bitVector);
+		}
+
+		int creatBitVector(String s){
+			int bitVector = 0;
+			for(char c : s.toCharArray()){
+				int x = getCharNumber(c);
+				bitVector = toggle(bitVector, x);
+			}
+			return bitVector;
+		}
+
+		int toggle(int bitVector, int index){
+			if (index < 0) return bitVector;
+
+			int mask = 1 << index;
+			if((bitVector & mask) == 0)
+				bitVector |= mask;
+			else
+				bitVector &= ~mask;
+			return bitVector;
+		}
+
+		boolean checkExactlyOneBitSet(int bitVector){
+			return ((bitVector - 1) & bitVector) == 0;
+		}
 	}
 }
--- src/main/java/com/java/adawang/MustHaveKnowledge/DataStructures/README.md
+++ src/main/java/com/java/adawang/MustHaveKnowledge/DataStructures/README.md
@@ -1,3 +1,13 @@
+## ArraysStrings
+
+1. Check if characters are unique in a given string. There are always two choices: HashMap or  BitVector(Vector`s length depends on character set).
+    Problems: IsUnique
+    
+2. Check if two strings have identical character counts. (1)Sort two strings then check. (2)Use a letter array to keep the character counts.
+    Problems: CheckPermutation
+
+
+
 Linked Lists
 Trees, Tries and Graphs
 Stacks and Queues
