2ee134be2b19a561b861b4b9ef6492a9eb506f49
yangwang000 <wymm.010@gmail.com>
2022-02-03 20:22:22 -0500

    update

--- src/main/java/com/java/adawang/LeetCode.java
+++ src/main/java/com/java/adawang/LeetCode.java
@@ -1,41 +1,65 @@
 package com.java.adawang;
-
-import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.*;
-
-import static org.junit.Assert.assertThrows;
-import static org.junit.Assert.assertTrue;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 
 public class LeetCode {
 
-	class Solution {
-		public List<Integer> findDisappearedNumbers(int[] nums) {
-			Arrays.sort(nums);
-			List<Integer> ans = new ArrayList<>();
-			int left = 0, right = 0;
-			while(right < nums.length){
-				if(nums[left] != nums[right]){
-					if(nums[right] - nums[left] > 1){
-						int gap = nums[right] - nums[left];
-						for(int i = 1; i < gap; i++){
-							ans.add(nums[left] + i);
-						}
-					}
-				}
-				left = right;
-				right++;
-			}
 
-			return ans;
+	   public class TreeNode {
+	       int val;
+	       TreeNode left;
+	       TreeNode right;
+	       TreeNode() {}
+	       TreeNode(int val) { this.val = val; }
+	       TreeNode(int val, TreeNode left, TreeNode right) {
+	           this.val = val;
+	           this.left = left;
+	           this.right = right;
+	       }
+	   }
+
+	class Solution {
+		int curId = 1;
+
+		public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
+			Map<String, Integer> serialToId = new HashMap<>();
+			Map<Integer, Integer> idToCount = new HashMap<>();
+			List<TreeNode> res = new LinkedList<>();
+			postorder(root, serialToId, idToCount, res);
+			return res;
 		}
+
+		private int postorder(TreeNode root, Map<String, Integer> serialToId, Map<Integer, Integer> idToCount, List<TreeNode> res) {
+			if (root == null) return 0;
+			int leftId = postorder(root.left, serialToId, idToCount, res);
+			int rightId = postorder(root.right, serialToId, idToCount, res);
+			String curSerial = leftId + "," + root.val + "," + rightId;
+			int serialId = serialToId.getOrDefault(curSerial, curId);
+			if (serialId == curId) curId++;
+			serialToId.put(curSerial, serialId);
+			idToCount.put(serialId, idToCount.getOrDefault(serialId, 0) + 1);
+			if (idToCount.get(serialId) == 2) res.add(root);
+			return serialId;
+		}
+
 	}
 
 	@Test
 	public void whenExceptionThrown_thenAssertionSucceeds() {
 		Solution s = new Solution();
-		List<Integer> ans = s.findDisappearedNumbers(new int[]{1,1});
-		System.out.println(ans);
+		TreeNode root = new TreeNode(1);
+		root.left = new TreeNode(2);
+		root.right = new TreeNode(3);
+		root.left.left = new TreeNode(4);
+		root.right.left = new TreeNode(2);
+		root.right.right = new TreeNode(4);
+		root.right.left.left = new TreeNode(4);
+		List<TreeNode> output = s.findDuplicateSubtrees(root);
+		for(TreeNode node : output)
+		    System.out.println(node.val);
 	}
 }
--- src/main/java/com/java/adawang/MustHaveKnowledge/DataStructures/README.md
+++ src/main/java/com/java/adawang/MustHaveKnowledge/DataStructures/README.md
@@ -131,3 +131,9 @@ Given an array of integers, return indices of the two numbers such that they add
 Given a string, find the first non-repeating character in it and return it's index. If it doesn't exist, return -1.
 ### Design the Key
 Given an array of strings, group anagrams together.
+1. When the order of each element in the string/array doesn't matter, you can use the **sorted** string/array as the key.
+2. If you only care about the offset of each value, usually the offset from the first value, you can use the **offset** as the key.
+3. In a tree, you might want to directly use the TreeNode as key sometimes. But in most cases, the **serialization of the subtree** might be a better idea.
+4. In a matrix, you might want to use **the row index or the column index** as key.
+5. In a Sudoku, you can combine the row index and the column index to identify which **block** this element belongs to.
+6. Sometimes, in a matrix, you might want to aggregate the values in the same **diagonal line**. 
